a5a7491431c42dbc4a123b060a0564304c711532
Atef Najar <atef@yimno.com>
2021-05-26 21:36:34 +0100

    Added API documentation.

---
+++ source/backend/src/main/kotlin/com/municipality/backend/infrastructure/documentation/OpenApiConfiguration.kt
@@ -0,0 +1,16 @@
+package com.municipality.backend.infrastructure.documentation
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfiguration {
+
+    @Bean
+    fun openAPI(): OpenAPI = OpenAPI()
+        .components(Components())
+        .info(Info().title("Municipality API").description(""))
+}
