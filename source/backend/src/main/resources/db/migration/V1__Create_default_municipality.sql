ee0ec846ce368925daf4f14e6aea5a424ae35d86
Atef Najar <atef@yimno.com>
2021-05-29 17:28:40 +0100

    Added municipality default migration.

---
+++ source/backend/src/main/resources/db/migration/V1__Create_default_municipality.sql
@@ -0,0 +1,2 @@
+INSERT INTO "muni"."municipality" ("id", "created_at", "created_by", "updated_at", "updated_by", "version", "name", "name_in_arabic")
+VALUES ('d36bccda-01a9-42d1-a39f-690cc45b0f6d', '2021-05-29 20:07:44.208987', 'atef', null, null, 1, 'Tunis', 'تونس');
